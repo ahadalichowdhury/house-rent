10f3a1137202c20c11e35cfab290129c61085570
Naimur <naeemhasan28@gmail.com>
2024-07-16 18:14:47 +0600

    update: success url

--- src/controller/paymentController.js
+++ src/controller/paymentController.js
@@ -26,7 +26,7 @@ exports.paymentApply = async (req, res) => {
       total_amount: price,
       currency: 'BDT',
       tran_id: trans_id, // use unique tran_id for each api call
-      success_url: `${FRONTEND_URL}/success-payment/${postId}`,
+      success_url: `${FRONTEND_URL}/success-payment?id=${postId}`,
       fail_url: `${FRONTEND_URL}/fail-payment`,
       cancel_url: `${FRONTEND_URL}/cancel-payment`,
       ipn_url: `${FRONTEND_URL}/ipn`,
